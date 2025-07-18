2e53939b3e0e6f02228c04fdab27f77356ec76d2
Damien Lachaume <135982616+dlachaume@users.noreply.github.com>
2025-07-18 15:16:13 +0200

    tmp

---
+++ .github/workflows/scripts/build-docker-image-references-json.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+echo "{"
+first=1
+for file in "$@"; do
+  if [[ ! -f "$file" ]]; then
+    echo "File not found: $file" >&2
+    exit 1
+  fi
+
+  filename=$(basename "$file")
+  component=${filename#docker-image-reference-}
+  component=${component%.txt}
+  if [ "$component" = "mithril-client-cli" ]; then
+    component="mithril-client"
+  fi
+
+  image_ref=$(<"$file")
+
+  if [ "$first" -eq 0 ]; then
+    echo ","
+  else
+    first=0
+  fi
+
+  printf '  "%s": "%s"' "$component" "$image_ref"
+done
+echo
+echo "}"
